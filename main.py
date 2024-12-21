f9997ad1e574625022526851ec7bd0041b022df6
Zelenskypilot <120605354+Zelenskypilot@users.noreply.github.com>
2024-12-21 22:50:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -38,4 +38,4 @@ def signup():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))  # Use the PORT environment variable or default to 5000
-    app.run(host='0.0.0.0', port=port, debug=False)  # Disable debug for production
+    app.run(host='0.0.0.0', port=port)  # Use Flask's built-in server (for testing)
